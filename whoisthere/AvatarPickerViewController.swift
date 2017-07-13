4e75f6cb250c9bbe811bc515ce26b09916240cf4
Efe Kocabas <efecemk@gmail.com>
2017-07-13 11:40:11 +0200

    extensions for delegates  added and some other minor changes

--- whoisthere/AvatarPickerViewController.swift
+++ whoisthere/AvatarPickerViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class AvatarPickerViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout
+class AvatarPickerViewController: UICollectionViewController
 {
-    let reuseIdentifier = "AvatarPickerCell"
+    let avatarCellReuseIdentifier = "AvatarPickerCell"
     let columnCount = 3
     let margin : CGFloat = 10
+    let avatarCount = 8
     
     
     // MARK: View lifecycle
@@ -22,33 +23,28 @@ class AvatarPickerViewController: UICollectionViewController, UICollectionViewDe
         
         ViewHelper.setCollectionViewLayout(collectionView: collectionView, margin: margin)
     }
-    
-    
-    // MARK: - UICollectionViewDataSource protocol
-    
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let flowLayout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
-        let marginsAndInsets = flowLayout.sectionInset.left + flowLayout.sectionInset.right + flowLayout.minimumInteritemSpacing * CGFloat(columnCount - 1)
-        let itemWidth = ((collectionView.bounds.size.width - marginsAndInsets) / CGFloat(columnCount)).rounded(.down)
-        return CGSize(width: itemWidth, height: itemWidth)
-    }
+}
+
+
+// MARK: - UICollectionViewDataSource protocol
+extension AvatarPickerViewController {
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 8
+        return avatarCount
     }
     
-    // make a cell for each cell index path
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! AvatarPickerCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: avatarCellReuseIdentifier, for: indexPath as IndexPath) as! AvatarPickerCell
         
         cell.avatarImageView.image = UIImage(named: String(format: "%@%d", Constants.kAvatarImagePrefix, indexPath.item + 1))
         
         return cell
     }
-    
-    // MARK: - UICollectionViewDelegate protocol
+}
+
+// MARK: - UICollectionViewDelegate protocol
+extension AvatarPickerViewController {
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
@@ -59,4 +55,15 @@ class AvatarPickerViewController: UICollectionViewController, UICollectionViewDe
         self.navigationController?.popViewController(animated: true)
         
     }
+}
+
+extension AvatarPickerViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let flowLayout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        let marginsAndInsets = flowLayout.sectionInset.left + flowLayout.sectionInset.right + flowLayout.minimumInteritemSpacing * CGFloat(columnCount - 1)
+        let itemWidth = ((collectionView.bounds.size.width - marginsAndInsets) / CGFloat(columnCount)).rounded(.down)
+        return CGSize(width: itemWidth, height: itemWidth)
+    }
+    
 }
