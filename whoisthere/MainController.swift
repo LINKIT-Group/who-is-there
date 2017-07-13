d7c0656b04e0c48f19f7319c09af7060a15cff5c
Efe Kocabas <efecemk@gmail.com>
2017-07-13 11:55:55 +0200

    main screen delegate methods are moved to the related extension

--- whoisthere/MainController.swift
+++ whoisthere/MainController.swift
@@ -9,24 +9,39 @@
 import CoreBluetooth
 import UIKit
 
-class MainController: UICollectionViewController, UICollectionViewDelegateFlowLayout, CBPeripheralManagerDelegate, CBCentralManagerDelegate, CBPeripheralDelegate {
+class MainController: UICollectionViewController {
     
-    var pManager = CBPeripheralManager()
+    var peripheralManager = CBPeripheralManager()
     var centralManager: CBCentralManager?
     var visiblePeripherals = Array<Peripheral>()
     var cachedPeripherals = Array<Peripheral>()
-    var cachedPNames = Dictionary<String, String>()
+    var cachedPeripheralNames = Dictionary<String, String>()
     var timer = Timer()
     
-    let reuseIdentifier = "MainCell"
+    let mainCellReuseIdentifier = "MainCell"
     let columnCount = 2
-    let margin : CGFloat = 10
     
     let WHOS_THERE_SERVICE_UUID = CBUUID(string: "4DF91029-B356-463E-9F48-BAB077BF3EF5")
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupProfileButton()
+        
+        centralManager = CBCentralManager(delegate: self, queue: DispatchQueue.main)
+        peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
+        
+        scheduledTimerWithTimeInterval()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        updateAdvertisingData()
+    }
+    
+    func setupProfileButton () {
         
         let rightButtonItem = UIBarButtonItem.init(
             title: "_profile_title".localized,
@@ -36,26 +51,13 @@ class MainController: UICollectionViewController, UICollectionViewDelegateFlowLa
         )
         
         self.navigationItem.rightBarButtonItem = rightButtonItem
-        
-        
-        centralManager = CBCentralManager(delegate: self, queue: DispatchQueue.main)
-        pManager = CBPeripheralManager(delegate: self, queue: nil)
-        
-        scheduledTimerWithTimeInterval()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        
-        updateAdvertisingData()
     }
     
     func rightButtonAction(sender: UIBarButtonItem) {
         
         let registerVC = self.storyboard?.instantiateViewController(withIdentifier: "RegisterController") as! RegisterViewController
         self.navigationController?.pushViewController(registerVC, animated: true)
-        
     }
-    
     
     func scheduledTimerWithTimeInterval(){
         
@@ -71,98 +73,18 @@ class MainController: UICollectionViewController, UICollectionViewDelegateFlowLa
     
     func updateAdvertisingData() {
         
-        if (pManager.isAdvertising) {
-            pManager.stopAdvertising()
+        if (peripheralManager.isAdvertising) {
+            peripheralManager.stopAdvertising()
         }
         
         let userData =  UserData()
         let advertisementData = String(format: "%@|%d|%d", userData.name, userData.avatarId, userData.colorId)
         
-        pManager.startAdvertising([CBAdvertisementDataServiceUUIDsKey:[WHOS_THERE_SERVICE_UUID], CBAdvertisementDataLocalNameKey: advertisementData])
-    }
-    
-    
-    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
-        
-        if (peripheral.state == .poweredOn){
-            
-            let transferService = CBMutableService(type: WHOS_THERE_SERVICE_UUID, primary: true)
-            pManager.add(transferService)
-            
-            updateAdvertisingData()
-        }
-        else {
-            // do something like alert the user that ble is not on
-        }
-    }
-    
-    func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        if (central.state == .poweredOn){
-            
-            self.centralManager?.scanForPeripherals(withServices: [WHOS_THERE_SERVICE_UUID], options: [CBCentralManagerScanOptionAllowDuplicatesKey : true])
-            
-        }
-        else {
-            // do something like alert the user that ble is not on
-        }
-    }
-    
-    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-        
-        //NSLog("%@", advertisementData)
-        
-        var peripheralName = cachedPNames[peripheral.identifier.description] ?? "unknown"
-        
-        if let advertisementName = advertisementData[CBAdvertisementDataLocalNameKey] as? String {
-            
-            peripheralName = advertisementName
-            cachedPNames[peripheral.identifier.description] = peripheralName
-        }
-        
-        let peripheral = Peripheral(item: peripheral, name: peripheralName)
-        
-        self.addOrUpdatePeripheralList(peripheral: peripheral, list: &visiblePeripherals)
-        self.addOrUpdatePeripheralList(peripheral: peripheral, list: &cachedPeripherals)
-    }
-    
-    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-        
-        peripheral.delegate = self
-        peripheral.discoverServices(nil)
-        
-    }
-    
-    func peripheral( _ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
-        
-        for service in peripheral.services! {
-            
-            peripheral.discoverCharacteristics(nil, for: service)
-        }
-    }
-    
-    func peripheral(
-        _ peripheral: CBPeripheral,
-        didDiscoverCharacteristicsFor service: CBService,
-        error: Error?) {        for characteristic in service.characteristics! {
-        let characteristic = characteristic as CBCharacteristic
-        
-        let data = "efe cem kocabas".data(using: .utf8)
-        peripheral.writeValue(data!, for: characteristic, type: CBCharacteristicWriteType.withResponse)
-        
-        
-        }
-    }
-    
-    func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveWrite requests: [CBATTRequest]) {
-        
-        let dataStr = String(data: requests[0].value!, encoding: String.Encoding.utf8) as String!
-        
-        AlertHelper.warn(delegate: self, message: dataStr!)
+        peripheralManager.startAdvertising([CBAdvertisementDataServiceUUIDsKey:[WHOS_THERE_SERVICE_UUID], CBAdvertisementDataLocalNameKey: advertisementData])
     }
     
     
     func addOrUpdatePeripheralList(peripheral: Peripheral, list: inout Array<Peripheral>) {
-        
         
         if !list.contains(where: { $0.item.identifier == peripheral.item.identifier }) {
             
@@ -181,19 +103,12 @@ class MainController: UICollectionViewController, UICollectionViewDelegateFlowLa
                     break
                 }
             }
-            
         }
     }
-    
-    // MARK: - UICollectionViewDataSource protocol
-    
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let flowLayout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
-        let marginsAndInsets = flowLayout.sectionInset.left + flowLayout.sectionInset.right + flowLayout.minimumInteritemSpacing * CGFloat(columnCount - 1)
-        let itemWidth = ((collectionView.bounds.size.width - marginsAndInsets) / CGFloat(columnCount)).rounded(.down)
-        return CGSize(width: itemWidth, height: itemWidth)
-    }
+}
+
+// MARK: - UICollectionViewDataSource protocol
+extension MainController {
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return visiblePeripherals.count
@@ -202,7 +117,7 @@ class MainController: UICollectionViewController, UICollectionViewDelegateFlowLa
     // make a cell for each cell index path
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! MainCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: mainCellReuseIdentifier, for: indexPath as IndexPath) as! MainCell
         
         let peripheral = visiblePeripherals[indexPath.row]
         
@@ -224,21 +139,103 @@ class MainController: UICollectionViewController, UICollectionViewDelegateFlowLa
         
         return cell
     }
-    
-    // MARK: - UICollectionViewDelegate protocol
+}
+
+// MARK: - UICollectionViewDelegate protocol
+extension MainController {
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
         let peripheral = visiblePeripherals[indexPath.row]
-        
         AlertHelper.warn(delegate: self, message: peripheral.name)
-        
-        //centralManager?.connect(peripheral.item, options: nil)
-        
+    }
+}
+
+extension MainController: UICollectionViewDelegateFlowLayout {
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let flowLayout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        let marginsAndInsets = flowLayout.sectionInset.left + flowLayout.sectionInset.right + flowLayout.minimumInteritemSpacing * CGFloat(columnCount - 1)
+        let itemWidth = ((collectionView.bounds.size.width - marginsAndInsets) / CGFloat(columnCount)).rounded(.down)
+        return CGSize(width: itemWidth, height: itemWidth)
+    }
+}
+
+extension MainController : CBCentralManagerDelegate {
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if (central.state == .poweredOn){
+            
+            self.centralManager?.scanForPeripherals(withServices: [WHOS_THERE_SERVICE_UUID], options: [CBCentralManagerScanOptionAllowDuplicatesKey : true])
+        }
     }
     
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        
+        var peripheralName = cachedPeripheralNames[peripheral.identifier.description] ?? "unknown"
+        if let advertisementName = advertisementData[CBAdvertisementDataLocalNameKey] as? String {
+            
+            peripheralName = advertisementName
+            cachedPeripheralNames[peripheral.identifier.description] = peripheralName
+        }
+        
+        let peripheral = Peripheral(item: peripheral, name: peripheralName)
+        
+        self.addOrUpdatePeripheralList(peripheral: peripheral, list: &visiblePeripherals)
+        self.addOrUpdatePeripheralList(peripheral: peripheral, list: &cachedPeripherals)
+    }
     
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        
+        peripheral.delegate = self
+        peripheral.discoverServices(nil)
+        
+    }
 }
+
+extension MainController : CBPeripheralDelegate {
+    
+    func peripheral( _ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        
+        for service in peripheral.services! {
+            
+            peripheral.discoverCharacteristics(nil, for: service)
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        
+        for characteristic in service.characteristics! {
+            let characteristic = characteristic as CBCharacteristic
+            
+            let data = "efe cem kocabas".data(using: .utf8)
+            peripheral.writeValue(data!, for: characteristic, type: CBCharacteristicWriteType.withResponse)
+        }
+    }
+}
+
+extension MainController : CBPeripheralManagerDelegate {
+
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        
+        if (peripheral.state == .poweredOn){
+            
+            let transferService = CBMutableService(type: WHOS_THERE_SERVICE_UUID, primary: true)
+            peripheralManager.add(transferService)
+            
+            updateAdvertisingData()
+        }
+    }
+    
+    func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveWrite requests: [CBATTRequest]) {
+        
+        let dataStr = String(data: requests[0].value!, encoding: String.Encoding.utf8) as String!
+        
+        AlertHelper.warn(delegate: self, message: dataStr!)
+    }
+}
+
+
 
 
 
